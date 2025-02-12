0cb145497ac4241fca4b0b63e038ed985a03a940
pipatko19 <154698339+Pipatko19@users.noreply.github.com>
2025-02-12 20:20:04 +0100

    changing to monorepo

---
+++ ball-bouncing/ball.py
@@ -0,0 +1,52 @@
+from PySide6 import QtWidgets as qtw
+from PySide6 import QtGui as qtg
+from PySide6 import QtCore as qtc
+from math import sin, cos, radians, pi
+
+class Ball:
+    def __init__(
+        self, 
+        x: int, y: int, 
+        speed: int = 0, angle: int | float= 0, 
+        color: qtg.QColor = qtg.QColor(255, 0, 0, 180), radius: int = 10
+        ):
+        
+        self.position = qtc.QPointF(x, y)
+        self.color = color
+        self.radius = radius
+        self.speed = speed
+        self._angle = radians(angle) #in degrees
+    
+    @property
+    def angle(self):
+        return self._angle
+    
+    @angle.setter
+    def angle(self, val):
+        #self._angle = val % (2 * pi)
+        self._angle = val
+    
+    def update(self):
+        dx = self.speed * cos(self.angle)
+        dy = self.speed * sin(self.angle)
+        self.position += qtc.QPointF(dx, dy)
+    
+    def reflect(self, axis=None):
+        """Reflects the angle. If axis=None, reflect both axis, if axis=0 reflect x, else reflect y"""
+        match axis:
+            case 0:
+                self.angle = -self.angle
+            case 1:
+                self.angle = pi - self.angle
+            case _:
+                self.angle = self.angle + pi
+    
+    def draw(self, painter: qtg.QPainter):
+        painter.setBrush(qtg.QBrush(self.color))
+        painter.drawEllipse(self.position, self.radius, self.radius)
+                
+    
+    
+
+if __name__ == '__main__':
+    ...
---
+++ ball-bouncing/wallpaper.py
@@ -0,0 +1,86 @@
+import sys
+import random
+
+from PySide6 import QtWidgets as qtw
+from PySide6 import QtGui as qtg
+from PySide6 import QtCore as qtc
+from ball import Ball
+
+BALL_RADIUS = 15
+OFFSET = BALL_RADIUS + 10
+BALL_COUNT = 200
+BALL_SPEED = 5
+
+class Wallpaper(qtw.QWidget):
+    key_pressed = qtc.Signal(str)
+    def __init__(self, geometry: qtc.QRect):
+        """Wallpaper constructor"""
+        super().__init__()
+        self.setGeometry(geometry)
+        #Main UI code goes here
+        
+        self.objects = []
+
+        self.balls = [
+            Ball(
+                x=random.randint(OFFSET, self.width() - OFFSET),
+                y=random.randint(OFFSET, self.height() - OFFSET),
+                speed=random.choice([-BALL_SPEED, BALL_SPEED]),
+                angle=random.randint(0, 359),
+                color=qtg.QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255), 180),
+                radius = BALL_RADIUS
+            )
+            for _ in range(BALL_COUNT)
+            ]
+        
+        self.objects.extend(self.balls)
+        
+        self.timer = qtc.QTimer()
+        self.timer.timeout.connect(self.update_all)
+        self.timer.start(30)
+        
+        self.key_pressed.connect(self.spin)
+        #End main UI code
+        self.show()
+    
+    def _update_ball_positions(self):
+        for ball in self.balls:
+            ball.update()
+            x, y = ball.position.x(), ball.position.y()
+            if x - ball.radius < 0 or x + ball.radius > self.width():
+                ball.reflect(1)
+            if y - ball.radius < 0 or y + ball.radius > self.height():
+                ball.reflect(0)
+    
+    def update_all(self):
+        self._update_ball_positions()
+        self.update()
+    
+    
+    def paintEvent(self, event):
+        painter = qtg.QPainter(self)
+        painter.setRenderHint(qtg.QPainter.Antialiasing)
+        
+        for obj in self.objects:
+            obj.draw(painter)
+        
+        return super().paintEvent(event)
+
+    def keyPressEvent(self, event):
+        key = event.text()
+        print(key)
+        if key == 'k' or key == 'l':
+            self.key_pressed.emit(key)
+        return super().keyPressEvent(event)
+
+    def spin(self, key):
+        print(key)
+        if key == 'k':
+            for ball in self.balls:
+                ball.reflect(0)
+        elif key == 'l':
+            for ball in self.balls:
+                ball.reflect(1)
+    
+if __name__ == '__main__':
+    ...
